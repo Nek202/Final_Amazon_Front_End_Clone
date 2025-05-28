d6ed17ca9f1eb3d1670cbe3c842fb8812289f67c
NekDevo <your-email@example.com>
2025-05-28 12:05:06 -0400

    update main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { DataProvider } from "./components/DataProvider/DataProvider.jsx";
-import {initialState,reducer} from './Utility/reducer.js'
+import { initialState, reducer } from "./Utility/reducer.js";
+import { BrowserRouter } from "react-router-dom"; // ✅ Import this
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <DataProvider reducer={reducer} initialState={initialState}>
-      <App />
-    </DataProvider>
+    <BrowserRouter basename="/Amazon_front_end_Clone">
+      {" "}
+      {/* ✅ Add this */}
+      <DataProvider reducer={reducer} initialState={initialState}>
+        <App />
+      </DataProvider>
+    </BrowserRouter>{" "}
+    {/* ✅ Close it here */}
   </StrictMode>
 );
