1429cddebc41720d3e9d73990a88eaba062bd769
Nek_Devloper <nekatibeb@yahoo.com>
2025-06-29 16:33:29 -0400

    Update axios.js

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/back-end-clone/us-central1/api",
-  baseURL: "http://localhost:2030/",
+  // baseURL: "http://localhost:2030/",
   //deployed version of amazon server render.com"//
   baseURL: "https://amazon-expressedbased-deploy.onrender.com/",
 });
