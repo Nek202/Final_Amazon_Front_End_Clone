8484996b3748aa5507208aedcc9de9cf168c085d
Nek_Devloper <nekatibeb@yahoo.com>
2025-06-29 17:21:13 -0400

    Update Product.jsx

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import ProductCard from "./ProductCard";
-import classes from "./product.module.css";
+import classes from "./Product.module.css";
 
 function Product() {
   const [products, setProducts] = useState([]);
